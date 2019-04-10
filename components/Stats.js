95f6ac357f01282549b9622783e44352cea461f8
ImenBenA <imen.benabderrahmen@esprit.tn>
2019-04-10 02:00:49 +0100

    statistics

--- components/Stats.js
+++ components/Stats.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { StyleSheet, View, Text, ImageBackground, Image } from 'react-native';
+import {
+  /*LineChart,
+  BarChart,
+  PieChart,*/
+  ProgressChart
+  //ContributionGraph
+} from 'react-native-chart-kit'
+
+import { Dimensions } from 'react-native'
+
+const screenWidth = Dimensions.get('window').width;
+
+const data = [0.4, 0.6, 0.8];
+
+const chartConfig = {
+  backgroundGradientFrom: '#1E2923',
+  backgroundGradientTo: '#08130D',
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2 // optional, default 3
+};
 
 class Stats extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <ProgressChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+        />
         <Text>Stats</Text>
       </View>
     );
--- navigation/navigation.js
+++ navigation/navigation.js
@@ -36,7 +36,7 @@ const App = createMaterialTopTabNavigator(
       },
     },),
     SiriWave: SiriWave,
-    Stats: ExerciceQuestions,
+    Stats: Stats,
     Settings: Settings,
   },
   {
